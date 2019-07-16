e7a453ca32a938f2768f2af59a338b63a122b9d9
Kenna <kennaalastair@gmail.com>
2019-07-16 12:11:23 -0700

    added comments

--- src/Timer.js
+++ src/Timer.js
@@ -4,6 +4,7 @@ import "./App.css";
 function Timer(props) {
     const [time, setTime] = useState(900);
     useEffect(() => {
+        //every second we rerender time
         setInterval(() => {
             setTime((newTime) => { return newTime - 1 });
         }, 1000);
