dca55f0bc8c86dbef58933bb37d2493428c418f8
Kenna <kennaalastair@gmail.com>
2019-07-16 11:28:57 -0700

    created Scoreboard component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 //TODO: STEP 1 - Import the useState hook.
 import React, { useState } from 'react';
 import "./App.css";
-import BottomRow from "./BottomRow";
 import Buttons from "./Buttons";
+import Scoreboard from "./Scoreboard";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeCount, homeScore] = useState(0);
-  const [awayCount, awayScore] = useState(0);
+  const [homeCount, setHomeScore] = useState(0);
+  const [awayCount, setAwayScore] = useState(0);
 
   return (
     <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeCount}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayCount}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <Buttons homeCount={homeCount} homeScore={homeScore} awayScore={awayScore} awayCount={awayCount}  />
+      <Scoreboard homeCount={homeCount} awayCount={awayCount} />
+      <Buttons homeCount={homeCount} setHomeScore={setHomeScore} setAwayScore={setAwayScore} awayCount={awayCount}  />
     </div>
   );
 }
